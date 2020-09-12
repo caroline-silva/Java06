d45659584d6f8f393c6146c82d3df82fdc92877a
ne_aps <58328252+caroline-silva@users.noreply.github.com>
2020-09-11 22:50:10 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,61 @@
+
+public class Aluno {
+	
+	private String nome;
+	private int matricula;
+	private double nota1;
+	private double nota2;
+	private double notaReavaliacao;
+	
+
+
+	//Construtor sem parametros
+	double calcularMedia(){
+		return (nota1 + nota2)/2;
+	}
+	
+	//Construtor com parametros
+	double calcularMediaFinal(){
+		if(calcularMedia() >= 6) {
+			return calcularMedia();
+		} else {
+			return (calcularMedia() + this.getNotaReavaliacao()) / 2;
+		}
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public double getNota1() {
+		return nota1;
+	}
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+	public double getNota2() {
+		return nota2;
+	}
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+	public double getNotaReavaliacao() {
+		return notaReavaliacao;
+	}
+	public void setNotaReavaliacao(double notaReavaliacao) {
+		this.notaReavaliacao = notaReavaliacao;
+	}
+	
+	
+
+
+}
